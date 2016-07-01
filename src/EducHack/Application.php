53d625a3d99cd6b1968b8da721f40288f3be67f5
rchoquet <romain.choquet@canaltp.fr>
2016-07-01 17:45:08 +0200

    fix oui.txt

--- src/EducHack/Application.php
+++ src/EducHack/Application.php
@@ -91,12 +91,14 @@ class Application extends BaseApplication
             return new Response('');
         });
 
+
         $this->get('/', function () {
             return $this['twig']->render('index.twig', array(
                 'probe_requests_since' => ['count' => $this['orm.em']->getRepository('EducHack:Position')->count()],
                 'probe_requests_by_phone_brand' => $this->getBrandSharing(),
                 'connexion_by_domain' => ['HTC' => 5, 'iPhone' => 10, 'Autres' => 13],
                 'nb_pr' => $this['orm.em']->getRepository('EducHack:DeviceSSID')->findSSIDByMac(),
+                'last_cdn_hits' => $this['orm.em']->getRepository('EducHack:CDNHit')->fetchLast()
             ));
         });
     }
