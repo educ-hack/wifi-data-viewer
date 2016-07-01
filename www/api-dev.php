94127c28e5f0585014cecb2347dea7718d1ad5b3
jbcrestot <jean-baptiste.crestot@canaltp.fr>
2016-07-01 12:32:34 +0200

    add twig

--- www/api-dev.php
+++ www/api-dev.php
@@ -9,4 +9,8 @@ $app = new EducHack\Application(array(
     'debug' => true,
 ));
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
 $app->run();
