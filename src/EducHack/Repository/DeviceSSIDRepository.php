959cf8c3b318ec05c545aadce2df03b7790e4c57
Julien Maulny <doubjulien@hotmail.fr>
2016-07-01 13:25:51 +0200

    add DeviceSSID repository doctrine, persist uniquely device mac and ssid

---
+++ src/EducHack/Repository/DeviceSSIDRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace EducHack\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use EducHack\Model\DeviceSSID;
+
+class DeviceSSIDRepository extends EntityRepository
+{
+    /**
+     * @param string $mac
+     * @param string $ssid
+     *
+     * @return DeviceSSID|null
+     */
+    public function findOneByMacAndSSID($mac, $ssid)
+    {
+        return $this->createQueryBuilder('device_ssid')
+            ->leftJoin('device_ssid.device', 'device')
+            ->leftJoin('device_ssid.ssid', 'ssid')
+            ->andWhere('device.mac = :mac')
+            ->andWhere('ssid.name = :ssid')
+            ->setParameters(array(
+                'mac' => $mac,
+                'ssid' => $ssid,
+            ))
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
--- src/EducHack/Service/ProbeRequestsPersister.php
+++ src/EducHack/Service/ProbeRequestsPersister.php
@@ -4,6 +4,8 @@ namespace EducHack\Service;
 
 use Doctrine\Common\Persistence\ObjectManager;
 use EducHack\Model\Device;
+use EducHack\Model\SSID;
+use EducHack\Model\DeviceSSID;
 
 class ProbeRequestsPersister
 {
@@ -27,13 +29,31 @@ class ProbeRequestsPersister
     {
         foreach (json_decode($logs) as $probe) {
             $device = $this->om->getRepository('EducHack:Device')->findOneByMac($probe->mac);
+            $ssid = $this->om->getRepository('EducHack:SSID')->findOneByName($probe->requested_ssid);
+            $deviceSSID = $this->om->getRepository('EducHack:DeviceSSID')->findOneByMacAndSSID(
+                $probe->mac,
+                $probe->requested_ssid
+            );
 
             if (null === $device) {
                 $device = new Device();
-
                 $device->setMac($probe->mac);
-
                 $this->om->persist($device);
+            }
+
+            if (null === $ssid) {
+                $ssid = new SSID();
+                $ssid->setName($probe->requested_ssid);
+                $this->om->persist($ssid);
+            }
+
+            if (null === $deviceSSID) {
+                $deviceSSID = new DeviceSSID();
+                $deviceSSID
+                    ->setDevice($device)
+                    ->setSsid($ssid)
+                ;
+                $this->om->persist($deviceSSID);
             }
         }
 
